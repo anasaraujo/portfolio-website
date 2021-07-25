baa0151fdf4bca761aa1efb7fa570cbd618b6d42
asaraujo <asaraujo.ndlx@gmail.com>
2021-07-25 21:22:55 +0100

    fix javascript and css

--- script/asaPortfolio.js
+++ script/asaPortfolio.js
@@ -1,19 +1,34 @@
-document.querySelector(document).ready(function(){ 
-        document.querySelector(window).scroll(function(){
-        if (this.scrollY > 20){
-            document.querySelector('.navbar').classList.add("sticky");
-        }else{
-            document.querySelector('.navbar').classList.remove("sticky");
-        }
-    });
-    //toggle menu/navbar script
-    document.querySelector('.menu-btn').click(function(){
-        document.querySelector('.navbar .menu').classList.toggle("active");
-        document.querySelector('.menu-btn i').classList.toggle("active");
-    });
 
-    // $('menu-btn').click(function(){
-    //     $('.navbar .menu').toggleClass("active");
-    //     $('.menu-btn i').toggleClass("active");
-    // });
+const navbar = document.querySelector(".navbar")
+const menuBtn = document.querySelector(".menu-btn")
+
+
+window.addEventListener("scroll", function(){
+    if (this.scrollY > 20){
+        navbar.classList.add("sticky");
+    }else{
+        navbar.classList.remove("sticky");
+    };
+        //toggle menu/navbar script  
+     menuBtn.addEventListener('click', function(){
+         document.querySelector('.navbar .menu').classList.toggle("active");
+         document.querySelector('.menu-btn i').classList.toggle("active");
+     });
 });
+
+
+
+/* $(document).ready(function(){
+     $(window).scroll(function(){
+         if(this.scrollY > 20){
+             $('.navbar').addClass("sticky");
+         }else{
+             $('.navbar').removeClass("sticky");
+         }
+     });
+    //toggle menu/navbar script
+     $('menu-btn').click(function(){
+         $('.navbar .menu').toggleClass("active");
+         $('.menu-btn i').toggleClass("active");
+    });
+}); */
