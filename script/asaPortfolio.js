a6b205880c75197b26d5708bbb2c65a0fb02a7c4
asaraujo <asaraujo.ndlx@gmail.com>
2021-08-01 04:14:02 +0100

    new background and added about section

--- script/asaPortfolio.js
+++ script/asaPortfolio.js
@@ -1,34 +1,24 @@
 
 const navbar = document.querySelector(".navbar")
 const menuBtn = document.querySelector(".menu-btn")
+const menuH = document.querySelector(".menu-h")
+const menuX = document.querySelector(".menu-x")
 
-
+//EventListener for scroll page menu/navbar 
 window.addEventListener("scroll", function(){
     if (this.scrollY > 20){
         navbar.classList.add("sticky");
     }else{
         navbar.classList.remove("sticky");
     };
-        //toggle menu/navbar script  
-     menuBtn.addEventListener('click', function(){
-         document.querySelector('.navbar .menu').classList.toggle("active");
-         document.querySelector('.menu-btn i').classList.toggle("active");
-     });
+
 });
 
 
-
-/* $(document).ready(function(){
-     $(window).scroll(function(){
-         if(this.scrollY > 20){
-             $('.navbar').addClass("sticky");
-         }else{
-             $('.navbar').removeClass("sticky");
-         }
-     });
-    //toggle menu/navbar script
-     $('menu-btn').click(function(){
-         $('.navbar .menu').toggleClass("active");
-         $('.menu-btn i').toggleClass("active");
-    });
-}); */
+//EventListener for toggle menu/navbar hamburguer 
+menuBtn.addEventListener('click', function(){
+    document.querySelector('.menu').classList.toggle("active");
+    // document.querySelector('.menu-btn').classList.toggle("active");
+    menuH.classList.toggle("off");
+    menuX.classList.toggle("off");
+});
